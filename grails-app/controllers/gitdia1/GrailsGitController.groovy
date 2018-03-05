c19f130277807a6996b072ed2f6d45eb048d05fb
Koppito <koppito123@gmail.com>
2018-03-05 11:47:15 -0300

    Agregado GrailsGitController

---
+++ grails-app/controllers/gitdia1/GrailsGitController.groovy
@@ -0,0 +1,6 @@
+package gitdia1
+
+class GrailsGitController {
+
+    def index() { }
+}
---
+++ src/test/groovy/gitdia1/GrailsGitControllerSpec.groovy
@@ -0,0 +1,18 @@
+package gitdia1
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class GrailsGitControllerSpec extends Specification implements ControllerUnitTest<GrailsGitController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
