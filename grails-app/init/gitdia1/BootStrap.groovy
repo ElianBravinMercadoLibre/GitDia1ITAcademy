8378ddb26b6388cee246e680ae2ecb9ac4c63718
Koppito <koppito123@gmail.com>
2018-03-05 14:10:28 -0300

    Agregado sitios y categorias al init

--- grails-app/init/gitdia1/BootStrap.groovy
+++ grails-app/init/gitdia1/BootStrap.groovy
@@ -17,6 +17,26 @@ class BootStrap {
         site3.name = "Site 3"
         site3.active = true
         site3.save()
+
+        Category category = new Category()
+        category.name = "AAA"
+        category.urlImage = "No Image"
+        category.site = site1
+        category.save()
+
+        Category category2 = new Category()
+        category2.name = "BBB"
+        category2.urlImage = "No Image"
+        category2.site = site2
+        category2.save()
+
+        Category category3 = new Category()
+        category3.name = "CCC"
+        category3.urlImage = "No Image"
+        category3.site = site3
+        category3.save()
+
+
     }
     def destroy = {
     }
